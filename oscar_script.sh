1a6897f8f0f5027c0ac03bf0feb4633352df3e80
obaeza16 <58266977+obaeza16@users.noreply.github.com>
2020-09-15 18:49:55 +0200

    Update oscar_script.sh

--- oscar_script.sh
+++ oscar_script.sh
@@ -1,42 +1,46 @@
 #!/bin/bash
- 
- 
-# set job name
- 
-#SBATCH --job-name=script_pleiofdr_oscar
 
 
 # set the partition where the job will run
 
 #SBATCH --partition=normal
- 
+
 
 # set the number of nodes
 
-#SBATCH --nodes=4
- 
+#SBATCH --nodes=6
+
+
+# set the name of the job
+
+#SBATCH --job-name=oscar_conjfdr
+
 
 # set max wallclock time
 
-#SBATCH --time=1:00:00
- 
+#SBATCH --time=2:00:00
+
+
+# set the requested memory
+
+#SBATCH --mem=40000
+
 
 # mail alert at start, end and abortion of execution
 
 #SBATCH --mail-type=ALL
 
-# ask for more memory
-
-#SBATCH --mem=60000
-
 
 # send mail to this address
 
 #SBATCH --mail-user=oscar.baeza@uvic.cat
- 
+
 
 # run the application
 
-module load MATLAB/2019b
+DIR=/homes/users/obaeza/scratch/pleiofdr/
 
-matlab -nosplash -nodesktop -nodisplay -r "run('/homes/scratch/pleioFDR/runme.m');"
+module load MATLAB/2016a
+
+matlab -nosplash -nodesktop -nodisplay -r "run('/homes/users/obaeza/scratch/pleiofdr/runme.m')"
+
